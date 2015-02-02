b9a40eccfe3108617dc85a2ed8fab4654544101e
Alex Jones <alex@jonesnet.tk>
2015-02-01 22:56:35 -0800

    still trying to get the curve port

--- test.php
+++ test.php
@@ -3,5 +3,14 @@
 include("nxtlib.php");
 
 $nxt = new Nxtlib("jnxt.org");
-echo $nxt->sign("test", "test");
+//echo $nxt->sign("test", "test");
+$that = curve25519_keygen([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+
+echo "<br/><br/>";
+var_dump($that);
+
 ?>
+<script src="curve25519.js"></script>
+<script>
+document.write(JSON.stringify(curve25519.keygen([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])));
+</script>
